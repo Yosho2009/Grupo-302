c4b37d8b77260f1f39669547222a99de0f2eef70
lili_nav <ln251933@gmail.com>
2025-12-10 10:09:21 -0600

    Creé y subo  archivo suma.py

---
+++ suma.py
@@ -0,0 +1,14 @@
+def sumar(a,b,c):
+    return a + b + c
+resultado= sumar(10,20,5)
+print(f"el resultado de la suma es:",resultado)
+
+def suma ():
+    numero1 = float(input("ingresa el primer numero"))
+    numero2 = float(input("ingresa el primer numero"))
+    numero3 = float(input("ingresa el primer numero"))
+    resultado = numero1 + numero2 + numero3
+    print(f"el resultado de la suma es:",resultado)
+if __name__=="__main__":
+    suma
+    
