3fa1db1c7a113c0f3b219cf5cfea3b3fccfcfd36
Yosho2009 <alonsozaid294@gmail.com>
2025-12-10 10:12:10 -0600

    multiplicacion de tres numeros

---
+++ grupo_302.py
@@ -0,0 +1,9 @@
+def suma():
+    print("Suma - Zaid")
+
+    n1 = int(input("Ingresa el primer numero: "))
+    n2 = int(input("ingresa el segundo numero: "))
+    n3 = int(input("ingresa el teercer numero"))
+
+    resultado = n1 + n2 + n3
+()
