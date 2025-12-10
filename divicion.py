caec7336e20790913936b9704def45d15e412206
lili_nav <ln251933@gmail.com>
2025-12-10 10:40:03 -0600

    subi archivo de divicion

---
+++ divicion.py
@@ -0,0 +1,13 @@
+def dividir(a,b,c):
+    return a / b / c
+resultado= dividir(100,4,5)
+print(f"el resultado de la divicion es:",resultado)
+
+def dividir ():
+    numero1 = float(input("ingresa el primer numero"))
+    numero2 = float(input("ingresa el primer numero"))
+    numero3 = float(input("ingresa el primer numero"))
+    resultado = numero1 + numero2 + numero3
+    print(f"el resultado de la divicion es:",resultado)
+if __name__=="__main__":
+    dividir
