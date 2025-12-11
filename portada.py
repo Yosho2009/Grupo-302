90618676355fe2370414cc3f71085a772050fc98
gustavohndz <gustavohndz03@gmail.com>
2025-12-11 08:21:29 -0600

    portada py

---
+++ portada.py
@@ -0,0 +1,6 @@
+#Codigo realizado por israel
+print("Equipo conformado por: GUSTAVO,ISRAEL,LILIANA,BARBARA,ALONSO")
+print("Codigos de las operaciones: +,-,*,/")
+print("Materia: DESARROLLA SOFTWARE CON HERRAMIENTAS ORIENTADAS A LA PRODUCTIVIDAD  ")
+print("Profesor: SERGIO IBAÑEZ MONTIEL ")
+print("GRUPO: 302      TURNO: MATUTINO")
