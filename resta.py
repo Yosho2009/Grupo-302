a29815d39d9ab3b87f2462bf257b6d36ef24c910
gustavohndz <gustavohndz03@gmail.com>
2025-12-11 07:50:34 -0600

    Resta py

---
+++ resta.py
@@ -0,0 +1,10 @@
+def restar(): 
+    numero1 = float(input("Ingresa el primer numero: "))
+    numero2 = float(input("Ingresa el segundo numero: "))
+    numero3=  float(input("Ingresa el tercer numero: "))
+
+    resultado = numero1 - numero2 - numero3
+    print("La resta es: ", resultado)
+
+if __name__=="__main__":
+    restar() 
